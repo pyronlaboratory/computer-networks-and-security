b608adc210971ce69822aada210b782c27607ac7
Ronyx <ronnie94official@gmail.com>
2019-02-18 08:15:19 +0530

    v1.2

--- diffie-hellman-algorithm.py
+++ diffie-hellman-algorithm.py
@@ -9,22 +9,25 @@ KR_b = 15
  
 # Initiate Key Exchange
 print("Publicly Shared Variables between P1 and P2>>")
-print("")
+
 print("Shared Prime: " , p)
 print("Shared Base:  " , g)
-print("") 
+
 # A = g^a mod p
 A = (g**KR_a) % p
 print("P1 Sends Over Public Channel: " , A)
+
 # B = g^b mod p
 B = (g**KR_b) % p
 print("P2 Sends Over Public Channel: ", B)
-print("") 
+
+# Generated Secret Key
 print("Privately Calculated Shared Secret>>")
-print("")
+
 # P1 Computes Shared Secret: s = B^a mod p
 K_a = (B ** KR_a) % p
 print("P1 Shared Secret: ", K_a)
+
 # P2 Computes Shared Secret: s = A^b mod p
 K_b = (A ** KR_b) % p
 print("P2 Shared Secret: ", K_b)
